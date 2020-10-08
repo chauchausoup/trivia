9bd500a4f1454f2147eced5ab1ae0cb34e459b6d
Krishna <7krishna7dhakal7@gmail.com>
2020-10-08 21:29:24 +0545

    eod

--- index.ts
+++ index.ts
@@ -1,39 +1,40 @@
-var app = require('express')();
-var http = require('http').createServer(app);
-var io = require('socket.io')(http);
+var app = require("express")();
+var http = require("http").createServer(app);
+var io = require("socket.io")(http);
 
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html');
+app.get("/home", (req, res) => {
+  res.sendFile(__dirname + "/static/home.html");
+});
+
+app.get("/setter", (req, res) => {
+  res.sendFile(__dirname + "/static/questions.html");
+});
+
+app.get("/answerer", (req, res) => {
+  res.sendFile(__dirname + "/static/answer.html");
 });
 
 
-io.on('connection', (socket) => {
-  console.log(`A user connected.`)
+io.on("connection", (socket) => {
+  //user is connected
+  console.log(`A user connected.`);
 
-  //broadcast
-  socket.broadcast.emit('hi');
-
-
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
-  })
-
-  socket.on('chat message', (msg) => {
-
-    io.emit('chat message', msg);
-    console.log(`Message: ${msg}`)
-    
-  })
+  //user is disconnected
+  socket.on("disconnect", () => {
+    console.log("user disconnected");
+  });
 
 
-})
-
+  socket.on("chat message", (msg) => {
+    io.emit("chat message", msg);
+    console.log(`Message: ${msg}`);
+  });
+});
 
 http.listen(3000, () => {
-  console.log('listening on *:3000');
+  console.log("listening on *:3000");
 });
-
-
-
