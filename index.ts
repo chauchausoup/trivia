74c0c73d078fb3af1cc9c15998a87d0a15bfa72a
Krishna <7krishna7dhakal7@gmail.com>
2020-10-08 17:37:57 +0545

    rough v1

---
+++ index.ts
@@ -0,0 +1,39 @@
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+
+
+app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+io.on('connection', (socket) => {
+  console.log(`A user connected.`)
+
+  //broadcast
+  socket.broadcast.emit('hi');
+
+
+
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+
+  socket.on('chat message', (msg) => {
+
+    io.emit('chat message', msg);
+    console.log(`Message: ${msg}`)
+    
+  })
+
+
+})
+
+
+http.listen(3000, () => {
+  console.log('listening on *:3000');
+});
+
+
+
