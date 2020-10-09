0ff23aabef7126be0cefa4e77815111d98598b8f
Krishna <7krishna7dhakal7@gmail.com>
2020-10-09 22:41:34 +0545

    antd

--- static/src/components/homePage.js
+++ static/src/components/homePage.js
@@ -1,22 +1,48 @@
 import React,{useState} from 'react';
-import {DatePicker,message} from 'antd';
+import {PageHeader,Carousel,Button} from 'antd';
 import 'antd/dist/antd.css';
 import '../index.css'
 
 
+
+const contentStyle = {
+    height: '60vh',
+    color: '#fff',
+    lineHeight: '50vh',
+    textAlign: 'center',
+    background: '#364d79',
+  };
+  
+
 const HomePage = ()=>{
-    const [date,setDate] =useState(null);
-    const handleChange=value=>{
-        message.info(`Selected Date: ${value?value.format('YYYY-MM-DD'):'None'}`)
-        setDate(value)
-    };
-    return(
-        <div style={{ width: 400, margin: '100px auto' }}>   
-            <DatePicker onChange={handleChange}/>
-            <div style={{ marginTop: 16 }}>
-                Selected Date: {date ? date.format('YYYY-MM-DD') : 'None'}
-             </div>
-        </div>
-    )
+   return(
+       <>
+    <PageHeader
+        className="site-page-header"
+        title="Trivia"
+    />
+
+    <Carousel effect="fade" autoplay>
+    <div>
+      <h1 style={contentStyle}>Trivia : Where minds come together...</h1>
+    </div>
+    <div>
+      <h1 style={contentStyle}>Fill ins</h1>
+    </div>
+    <div>
+      <h1 style={contentStyle}>MCQs</h1>
+    </div>
+   
+  </Carousel>
+  <div className="setter-button">
+  </div>
+    <Button default>Setter</Button>
+  <div className="answerer-button">
+    <Button default>Answerer</Button>
+  </div>
+</>
+   )
 }
 export default HomePage;
+
+
