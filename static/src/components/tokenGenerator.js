32cf18add84745c855276c8cfa81b75538864eb2
Krishna <7krishna7dhakal7@gmail.com>
2020-10-17 21:37:41 +0545

    paused untill thursday

--- static/src/components/tokenGenerator.js
+++ static/src/components/tokenGenerator.js
@@ -1,7 +1,6 @@
 import React, { useState } from "react";
-import { Radio, Input } from 'antd';
+import { Radio, Input } from "antd";
 import { useHistory } from "react-router-dom";
-
 
 export default function TokenGenerator() {
   var history = useHistory();
@@ -9,8 +8,7 @@ export default function TokenGenerator() {
   const [st, setST] = useState("");
   const [at, setAT] = useState("");
   const [maxUsers, setMaxUsers] = useState(0);
-  const[etValue,setValue]=useState(1);
-
+  const [etValue, setValue] = useState(1);
 
   const handleST = (e) => {
     e.preventDefault();
@@ -24,31 +22,28 @@ export default function TokenGenerator() {
     e.preventDefault();
     setMaxUsers(e.target.value);
   };
-  
-  const onChange = e => {
-    console.log('radio checked', e.target.value);
-    setValue(e.target.value)
+
+  const onChange = (e) => {
+    console.log("radio checked", e.target.value);
+    setValue(e.target.value);
   };
 
-    const radioStyle = {
-      display: 'block',
-      height: '30px',
-      lineHeight: '30px',
-    };
+  const radioStyle = {
+    display: "block",
+    height: "30px",
+    lineHeight: "30px",
+  };
 
+  const submitHandler = (e) => {
+    e.preventDefault();
+    history.push(`/screen/`);
+    console.log(st, at, maxUsers, etValue);
+  };
 
-    const submitHandler=(e)=>{
-      e.preventDefault()
-      history.push(`/screen/`)
-    console.log(st,at,maxUsers,etValue)
-  }
-
-/* 
+  /* 
 WORK_LEFT:
 validating the input fields as per the specifications
 */
-
-
 
   return (
     <form onSubmit={submitHandler}>
@@ -63,30 +58,27 @@ validating the input fields as per the specifications
       <br />
       <label>Expiry Time:</label>
       <br />
-      
+
       {/* here comes the antd radio selector */}
 
       <Radio.Group onChange={onChange} value={etValue}>
-          <Radio style={radioStyle} value={"min45"}>
-            45 minutes
-          </Radio>
-          <Radio style={radioStyle} value={"hrs2"}>
-            2 hrs
-          </Radio>
-          <Radio style={radioStyle} value={"hrs4"}>
-            4 hrs
-          </Radio>
-          <Radio style={radioStyle} value={"hrs6"}>
-            6 hrs
-          </Radio>
-        </Radio.Group>
+        <Radio style={radioStyle} value={"min45"}>
+          45 minutes
+        </Radio>
+        <Radio style={radioStyle} value={"hrs2"}>
+          2 hrs
+        </Radio>
+        <Radio style={radioStyle} value={"hrs4"}>
+          4 hrs
+        </Radio>
+        <Radio style={radioStyle} value={"hrs6"}>
+          6 hrs
+        </Radio>
+      </Radio.Group>
 
       <br />
       <br />
-      <input type="submit" value="Lets Go..."/>
+      <input type="submit" value="Lets Go..." />
     </form>
   );
 }
-
-
-
